eab50270c4c5463fc73d8c404efdd57cdccfb248
Robin Bretin <rbretin@ensc.fr>
2018-11-14 10:40:35 +0100

    Fusion projets

--- partie 2/Pluscourtchemin/Class1.cs
+++ partie 2/Pluscourtchemin/Class1.cs
@@ -21,21 +21,21 @@ namespace Pluscourtchemin
         public override double GetArcCost(GenericNode N2)
         {
             Node2 N2bis = (Node2)N2;
-            return Form1.matrice[numero, N2bis.numero];
+            return FormD.matrice[numero, N2bis.numero];
         }
 
         public override bool EndState()
         {
-            return (numero == Form1.numfinal);
+            return (numero == FormD.numfinal);
         }
 
         public override List<GenericNode> GetListSucc()
         {
             List<GenericNode> lsucc = new List<GenericNode>();
 
-            for (int i=0; i < Form1.nbnodes; i++)
+            for (int i=0; i < FormD.nbnodes; i++)
             {
-                if (Form1.matrice[numero, i] != -1)
+                if (FormD.matrice[numero, i] != -1)
                 {
                     Node2 newnode2 = new Node2();
                     newnode2.numero = i;
